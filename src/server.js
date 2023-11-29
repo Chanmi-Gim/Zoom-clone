b668513be485423c7f0ecbd614274242ff7e992d
Chanmi-Gim <beatres@gachon.ac.kr>
2023-11-29 13:52:43 +0900

    feat : websocket 이용하여 브라우저, 백엔드, 프론트간 통신  - 브라우저 → 백엔드 통신  - 브라우저 → 프론트 통신  - 백엔드 ↔ 프론트 통신

--- src/server.js
+++ src/server.js
@@ -1,10 +1,27 @@
+import http from "http"
 import express from "express";
-
+import WebSocket from "ws";
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+const server = http.createServer(app)
+const wss = new WebSocket.Server({server})
+
+wss.on("connection", (socket)=>{ 
+    console.log("Connected to Browser ✅");
+    socket.on("close", ()=> {console.log("Disconnected from the Browser ❌")})
+    socket.on("message",(message)=>{
+        socket.send(message.toString('utf8'));
+    });
+    // socket.on("message",(message)=>{console.log(message.toString('utf8'));})
+    // socket.send("hello! 👋🏻");
+})
+
+server.listen(3000, handleListen);
+
