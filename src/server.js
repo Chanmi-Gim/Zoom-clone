3c38721eb6179cd3f7c52318cf6860ff1a9d5068
Chanmi-Gim <beatres@gachon.ac.kr>
2023-12-05 13:18:12 +0900

    feat : 메세지 전송 알림 - Room에 입/퇴장한 유저 브로드캐스트 - 닉네임 입력폼 생성 - 유저가 입력한 Message 브로드캐스트(닉네임 포함)

--- src/server.js
+++ src/server.js
@@ -17,60 +17,23 @@ const wsServer = SocketIO(httpServer);
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
 wsServer.on("connection", (socket) => {
+    socket['nickname'] = 'Anon'
     socket.onAny((event)=> {
         console.log(`Socket Event: ${event}`);
     })
     socket.on("enter_room", (roomName, done)=> {
         socket.join(roomName);
         done();
-        // setTimeout(() => {
-        //     done("hello from the backend");  
-        // }, 5000)
+        socket.to(roomName).emit("welcome", socket.nickname);
     }); 
-    // socket.on("enter_room", (msg, done)=> {
-    //     console.log(msg);
-    //     setTimeout(()=>{
-    //         done(); // back-end에서 실행시켜서 front-end에서 코드실행.
-    //     },10000)
-    // }); 
+    socket.on("disconnecting", ()=>{
+        socket.rooms.forEach((room)=> {
+            socket.to(room).emit("bye", socket.nickname)
+        });
+    })
+    socket.on("new_message", (msg, room, done)=>{
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`)
+        done()
+    })
+    socket.on('nickname', nickname => socket['nickname']= nickname)
 });
-
-
-
-// WebSocket Code
-// import http from "http"
-// import express from "express";
-// import WebSocket from "ws";
-// const app = express();
-
-// app.set("view engine", "pug");
-// app.set("views", __dirname + "/views");
-// app.use("/public", express.static(__dirname + "/public"));
-// app.get("/", (req, res) => res.render("home"));
-// app.get("/*", (req, res) => res.redirect("/"));
-
-// const handleListen = () => console.log(`Listening on http://localhost:3000`);
-// const server = http.createServer(app)
-// const wss = new WebSocket.Server({server})
-
-// const sockets = [];
-// wss.on("connection", (socket)=>{ 
-//     sockets.push(socket);
-//     socket['nickname'] = "Anon"
-//     console.log("Connected to Browser ✅");
-//     socket.on("close", ()=> {console.log("Disconnected from the Browser ❌")})
-//     socket.on("message",(msg)=>{
-//         const message = JSON.parse(msg)
-//         switch(message.type){
-//             case "new_message":
-//                 sockets.forEach((aSocket)=> aSocket.send(`${socket.nickname} : ${message.payload.toString('utf8')}`));
-//                 break
-//             case "nickname":
-//                 socket["nickname"] = message.payload;
-//                 break
-//         }
-//     });
-// })
-
-// server.listen(3000, handleListen);
-
