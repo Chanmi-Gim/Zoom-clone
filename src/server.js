1ceb1c42a01c1c2a7e5aebf65706dbb21df5010e
Chanmi-Gim <beatres@gachon.ac.kr>
2023-12-05 15:28:21 +0900

    feat : 방인원 표출기능

--- src/server.js
+++ src/server.js
@@ -26,6 +26,11 @@ function publicRooms(){
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
+
+function countRoom(roomName){
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size
+}
+
 wsServer.on("connection", (socket) => {
     socket['nickname'] = 'Anon'
     socket.onAny((event)=> {
@@ -35,12 +40,12 @@ wsServer.on("connection", (socket) => {
     socket.on("enter_room", (roomName, done)=> {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome", socket.nickname);
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
         wsServer.sockets.emit("room_change", publicRooms())
     }); 
     socket.on("disconnecting", ()=>{
         socket.rooms.forEach((room)=> {
-            socket.to(room).emit("bye", socket.nickname)
+            socket.to(room).emit("bye", socket.nickname, countRoom(room)-1)
         });
     })
     socket.on("disconnect", ()=>{
